c8d8fde8b8976b1c91509d2e83d149c5ca69beb4
Stritixx <stritixxy@gmail.com>
2024-12-07 22:40:09 +0100

    vuesax

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuesax from 'vuesax-alpha';
+import 'vuesax-alpha/theme-chalk/index.css'
+import 'vuesax-alpha/theme-chalk/dark/css-vars.css'
 
 
 const app = createApp(App);
 app.use(router);
+app.use(Vuesax);
 app.mount('#app');
